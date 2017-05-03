aea60257c67fa928de42eac6579200b1844722b4
Hubolex <djhubolex1@gmail.com>
2017-05-03 23:43:57 +0200

    Base edit
    Edition code to improve performance and future UIDs.

--- gate_open.ino
+++ gate_open.ino
@@ -1,37 +1,46 @@
-#include <SPI.h>
-#include <MFRC522.h>
+#include <SPI.h>  //Serial Peripheral Interface (serial data protocol)
 
-#define czas_trwania 5000
-const byte UID[] = {0xB5, 0x61, 0x2B, 0x77};
+#include <MFRC522.h>  //rfid
+#include <MFRC522Extended.h>  //rfid
+
+#define czas_trwania 20
+
+const byte UID1[] = {0xB5, 0x61, 0x2B, 0x77};
 
 MFRC522 rfid(10, 9);
 MFRC522::MIFARE_Key key;
 boolean stan = false;
 unsigned long czas;
 
-void setup() {
+
+void setup() 
+{
   Serial.begin(9600);
   SPI.begin();
   rfid.PCD_Init();
   pinMode(2, OUTPUT);
 }
 
-void loop() {
+
+void loop() 
+{
   if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial())
   {
-    if (rfid.uid.uidByte[0] == UID[0] && 
-        rfid.uid.uidByte[1] == UID[1] &&
-        rfid.uid.uidByte[2] == UID[2] &&
-        rfid.uid.uidByte[3] == UID[3])
+    if (rfid.uid.uidByte[0] == UID1[0] &&
+        rfid.uid.uidByte[1] == UID1[1] &&
+        rfid.uid.uidByte[2] == UID1[2] &&
+        rfid.uid.uidByte[3] == UID1[3])
     {
       Serial.println("Poprawny");
       stan = true;
       czas = millis() + czas_trwania;
-    } else
+    }
+    else
     {
       Serial.println("Niepoprawny");
       stan = false;
     }
+    
     rfid.PICC_HaltA();
     rfid.PCD_StopCrypto1();
   }
@@ -39,5 +48,7 @@ void loop() {
   if (stan && czas < millis())
     stan = false;
 
-  digitalWrite(2, stan)  ;
+  digitalWrite(2, stan);
+  delay(1000);
 }
+
